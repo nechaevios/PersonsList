0d1471c39fd99e07ad12d652ccd9547aac8dd053
Nechaev S <{ID}+{username}@users.noreply.github.com>
2021-09-08 10:08:51 +0300

    Initial Commit

---
+++ PersonsList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonsList
+//
+//  Created by Nechaev Sergey  on 08.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
